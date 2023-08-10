f0616f14299a67340a2ed1e64cb2c555ca380800
imzeze <67260437+imzeze@users.noreply.github.com>
2023-08-10 12:30:01 +0900

    feat: styled sample

--- src/components/atoms/Text.tsx
+++ src/components/atoms/Text.tsx
@@ -1,42 +1,10 @@
-import React from 'react';
-import { Text as StyledText, StyleSheet, StyleProp, TextStyle } from 'react-native';
+import { TextStyle } from 'react-native';
+import { styled } from 'styled-components/native';
 
-interface TextProps {
-  children?: React.ReactNode;
-  type?: TextType;
-}
+const Text = styled.Text<TextStyle>`
+  ${({ color }) => color && `color: ${color.toString()};`}
+  ${({ fontSize }) => fontSize && `font-size: ${fontSize}`}
+  ${({ fontFamily }) => fontFamily && `font-family: ${fontFamily};`}
+`;
 
-type TextType = 'title' | 'buttonText' | 'default';
-
-const styles = StyleSheet.create({
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
-    textAlign: 'center',
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 16,
-    textAlign: 'center',
-  },
-  default: {
-    fontSize: 14,
-    color: '#000',
-  },
-});
-
-const getTextStyle = (type: TextType): StyleProp<TextStyle> => {
-  switch (type) {
-    case 'title':
-      return styles.title;
-    case 'buttonText':
-      return styles.buttonText;
-    default:
-      return styles.default;
-  }
-};
-
-export const Text: React.FC<TextProps> = ({ children, type = 'default' }) => {
-  return <StyledText style={getTextStyle(type)}>{children}</StyledText>;
-};
+export default Text;
