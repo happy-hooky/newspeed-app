cc64736f276e6e56989c4882a5b55e5b4f18b6ba
Bellhoon-__- <ilbora@naver.com>
2023-08-13 14:01:37 +0900

    🩹 fix: 불필요한 페이지 삭제

--- src/components/pages/index.tsx
+++ src/components/pages/index.tsx
@@ -2,6 +2,5 @@ import LoginPage from './Login';
 import MainPage from './Main';
 import ProfilePage from './Profile';
 import SearchPage from './Search';
-import SignInPage from './SignIn';
 
-export { LoginPage, MainPage, ProfilePage, SearchPage, SignInPage };
+export { LoginPage, MainPage, ProfilePage, SearchPage };
