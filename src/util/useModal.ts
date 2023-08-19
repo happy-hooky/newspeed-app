854fce8812cb47e8d57ac86186d5ce733b4daa70
imzeze <jihyekim019@gmail.com>
2023-08-19 18:46:24 +0900

    feat: Modal 생성

---
+++ src/util/useModal.ts
@@ -0,0 +1,33 @@
+import { Modals } from '@/components/modal';
+import { ModalState } from '@/recoil/system';
+import { useSetRecoilState } from 'recoil';
+
+interface onModalArgs {
+  type: keyof typeof Modals;
+  payload?: { [key: string]: any };
+}
+
+export const useModal = () => {
+  const setModalState = useSetRecoilState(ModalState);
+  const hash = Math.random().toString(36).substring(2, 11) + `${new Date().getTime()}`;
+
+  const onModal = ({ type, payload }: onModalArgs) => {
+    setModalState((prev) => [
+      ...prev,
+      {
+        hash,
+        type,
+        payload,
+      },
+    ]);
+  };
+
+  const closeModal = () => {
+    setModalState((prev) => prev.filter((curr) => curr.hash !== hash));
+  };
+
+  return {
+    onModal,
+    closeModal,
+  };
+};
