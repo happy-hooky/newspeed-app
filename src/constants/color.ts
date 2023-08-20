d2f7298c419f84c97dfc49fb256742efaa547e1c
Bellhoon-__- <ilbora@naver.com>
2023-08-21 08:20:35 +0900

    ✨ feat: peach 컬러 추가
    + 알파벳순정렬했습니다.

--- src/constants/color.ts
+++ src/constants/color.ts
@@ -1,10 +1,10 @@
 export const color = {
-  primary: '#FFB677',
-  papaya: '#FFDBBF',
-  goldenGrow: '#352E1E',
-  white: '#FFFFFF',
-  cheese: '#FFB677',
-  twilight: '#EFF0EB',
   black: '#000000',
+  cheese: '#FFB677',
+  goldenGrow: '#352E1E',
+  papaya: '#FFDBBF',
   peach: '#FFF7F1',
+  primary: '#FFB677',
+  twilight: '#EFF0EB',
+  white: '#FFFFFF',
 };
