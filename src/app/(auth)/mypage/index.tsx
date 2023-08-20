d47bde06725b81ef367c87541ee49406166079fe
Bellhoon-__- <ilbora@naver.com>
2023-08-21 08:19:32 +0900

    ✨ feat: 마이페이지 헤더옵션 추가.

--- src/app/(auth)/mypage/index.tsx
+++ src/app/(auth)/mypage/index.tsx
@@ -1,8 +1,17 @@
 import { ProfilePage } from '@/components/pages';
+import { color } from '@/constants';
+import { Stack } from 'expo-router';
 
 export default () => {
   return (
     <>
+      <Stack.Screen
+        options={{
+          title: '마이페이지',
+          headerShadowVisible: false,
+          headerTintColor: color.black,
+        }}
+      />
       <ProfilePage />
     </>
   );
