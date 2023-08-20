c846c82850559e7b23a459cdc98cee03b8d6571d
Bellhoon-__- <ilbora@naver.com>
2023-08-21 08:22:32 +0900

    🩹 fix : Expected style width to contain units Warning

--- src/components/atoms/TextInput.tsx
+++ src/components/atoms/TextInput.tsx
@@ -1,12 +1,6 @@
 import { TextStyle } from 'react-native';
 import styled from 'styled-components/native';
 
-const TextInput = styled.TextInput<TextStyle>`
-  padding: 10px;
-  height: 40,
-  borderColor: 'gray',
-  borderWidth: 1,
-  padding: 10,
-`;
+const TextInput = styled.TextInput<TextStyle>``;
 
 export default TextInput;
