db2c3abd5b433148eae60c1af9ed1cdd383cb329
imzeze <jihyekim019@gmail.com>
2023-11-25 14:47:18 +0900

    feat: font 적용 및 asset import 방식 개선

--- src/components/atoms/Image.tsx
+++ src/components/atoms/Image.tsx
@@ -1,25 +1,11 @@
 import { ImageStyle, Image as _Image } from 'react-native';
-import image from '../../../assets/image';
-
-const ImageSource = {
-  back: image['back'],
-  search: image['search'],
-  user: image['user'],
-  logo: image['logo'],
-  youtube: image['youtube'],
-  instagram: image['instagram'],
-  delete: image['delete'],
-  star: image['star'],
-  starFill: image['starFill'],
-  userMockImage: image['userMockImage'],
-};
-
+import ASSETS from '../../../assets';
 interface ImageProps extends ImageStyle {
-  src: keyof typeof ImageSource;
+  src: keyof typeof ASSETS;
 }
 
 const Image = ({ src, ...style }: ImageProps) => {
-  return <_Image source={ImageSource[src]} style={style} />;
+  return <_Image source={ASSETS[src]} style={style} />;
 };
 
 export default Image;
