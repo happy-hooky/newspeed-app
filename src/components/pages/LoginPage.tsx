40504e80024ff45c6edd001ba4bf9c570773738b
imzeze <jihyekim019@gmail.com>
2023-11-28 22:55:06 +0900

    feat: login page css 수정

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -8,60 +8,71 @@ const LoginPage = () => {
   const insets = useSafeAreaInsets();
 
   return (
-    <View flex={1} height="100%" justifyContent="flex-end" alignItems="center">
-      <View
-        paddingHorizontal={28}
-        paddingVertical={80}
-        alignItems="flex-start"
-        width="100%"
-        zIndex={1}
-      >
-        <Text fontSize={48} fontFamily="bold">
-          Newspeed
-        </Text>
-        <Text fontSize={16} marginTop={12}>
-          보고싶은 콘텐츠, 뉴스피드가 찾아드릴게요.
-        </Text>
-        <Text fontSize={16} marginTop={8}>
-          관심사 등록을 위해 로그인해주세요.
-        </Text>
-      </View>
-      <View
-        zIndex={1}
-        alignItems="center"
-        backgroundColor="rgba(255, 255, 255, 0.7)"
-        paddingHorizontal={40}
-        paddingTop={60}
-        paddingBottom={40}
-        borderRadius={20}
-        marginBottom={insets.bottom + 60}
-      >
-        <Button>
+    <View width="100%" height="100%" justifyContent="space-between" alignItems="flex-start">
+      <View zIndex={1} paddingHorizontal={28} marginTop={insets.top + 40}>
+        <Button onPress={() => router.replace('/(tabs)/main')}>
           <View
-            width={300}
-            height={50}
-            backgroundColor="#FEE500"
-            borderRadius={7}
-            flexDirection="row"
+            backgroundColor="rgba(255, 255, 255, 0.7)"
+            borderRadius={22}
+            width={44}
+            height={44}
             alignItems="center"
-            paddingHorizontal={14}
+            justifyContent="center"
           >
-            <Image src="kakaoLogo" width={22} height={22} />
-            <View flex={1} justifyContent="center" alignItems="center">
-              <Text color="rgba(0, 0, 0, 0.85)" fontSize={21} fontFamily="medium">
-                카카오 로그인
-              </Text>
-            </View>
+            <Image src="back" width={20} height={20} />
           </View>
         </Button>
-        <Image src="appleSignIn" width={300} height={60} marginVertical={20} resizeMode="contain" />
-        <Button onPress={() => router.replace('/(tabs)/main')}>
-          <Text>돌아가기</Text>
-        </Button>
+      </View>
+      <View zIndex={1} width="100%" paddingHorizontal={28}>
+        <View paddingVertical={40} width="100%">
+          <Text fontSize={48} fontFamily="bold">
+            Newspeed
+          </Text>
+          <Text fontSize={16} marginTop={12} lineHeight={24}>
+            보고싶은 콘텐츠, 뉴스피드가 찾아드릴게요. {'\n'}관심사 등록을 위해 로그인해주세요.
+          </Text>
+        </View>
+        <View width="100%" alignItems="center" marginBottom={insets.bottom + 20}>
+          <View
+            width="100%"
+            height={300}
+            borderRadius={8}
+            paddingTop={20}
+            alignItems="center"
+            justifyContent="center"
+            backgroundColor="rgba(255, 255, 255, 0.6)"
+          >
+            <Button>
+              <View
+                width={300}
+                height={50}
+                backgroundColor="#FEE500"
+                borderRadius={7}
+                flexDirection="row"
+                alignItems="center"
+                paddingHorizontal={14}
+              >
+                <Image src="kakaoLogo" width={22} height={22} />
+                <View flex={1} justifyContent="center" alignItems="center">
+                  <Text color="rgba(0, 0, 0, 0.85)" fontSize={21} fontFamily="medium">
+                    카카오 로그인
+                  </Text>
+                </View>
+              </View>
+            </Button>
+            <Image
+              src="appleSignIn"
+              width={300}
+              height={60}
+              marginVertical={20}
+              resizeMode="contain"
+            />
+          </View>
+        </View>
       </View>
       <LinearGradient
         style={{ position: 'absolute', width: '100%', height: '100%', top: 0, left: 0 }}
-        colors={['rgba(228, 108, 124, 0.4)', 'rgba(237, 166, 37, 0.4)', 'rgba(211, 204, 24, 0.4)']}
+        colors={['rgba(228, 108, 124, 0.3)', 'rgba(237, 166, 37, 0.3)', 'rgba(211, 204, 24, 0.3)']}
       />
     </View>
   );
